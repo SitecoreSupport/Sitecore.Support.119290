cccedebfb9d62dbcf3a122065aa8d9aa12ca5cb4
Oleg Braginskyy <obr@sitecore.net>
2016-12-13 16:34:47 +0200

    Initial

---
+++ src/Sitecore.Support.119290/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.119290")]
+[assembly: AssemblyProduct("Sitecore.Support.119290")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.119290/StandardValuesProvider.cs
@@ -0,0 +1,126 @@
+﻿using Sitecore.Data;
+using Sitecore.Caching;
+using Sitecore.Collections;
+using Sitecore.Common;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Data.LanguageFallback;
+using Sitecore.Data.Managers;
+using Sitecore.Data.Templates;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Support.Data
+{
+    public class StandardValuesProvider : Sitecore.Data.StandardValuesProvider
+    {
+        public override string GetStandardValue(Field field)
+        {
+            Assert.ArgumentNotNull(field, "field");
+            if (field.ID == FieldIDs.SourceItem || field.ID == FieldIDs.Source)
+            {
+                return string.Empty;
+            }
+            SafeDictionary<ID, string> standardValues = this.GetStandardValues(field.Item);
+            if (standardValues == null)
+            {
+                return null;
+            }
+            return standardValues[field.ID];
+        }
+
+        private SafeDictionary<ID, string> GetStandardValues(Item item)
+        {
+            if (ID.IsNullOrEmpty(item.TemplateID))
+            {
+                return null;
+            }
+            SafeDictionary<ID, string> safeDictionary = this.GetStandardValuesFromCache(item);
+            if (safeDictionary != null)
+            {
+                return safeDictionary;
+            }
+            safeDictionary = this.ReadStandardValues(item.TemplateID, item.Database, item.OriginalLanguage);
+            this.AddStandardValuesToCache(item, safeDictionary);
+            return safeDictionary;
+        }
+
+        private SafeDictionary<ID, string> GetStandardValuesFromCache(Item item)
+        {
+            StandardValuesCache standardValuesCache = item.Database.Caches.StandardValuesCache;
+            if (item.IsFallback)
+                return standardValuesCache.GetStandardValues(item.GetFallbackItem());
+            return standardValuesCache.GetStandardValues(item);
+
+        }
+
+        private void AddStandardValuesToCache(Item item, SafeDictionary<ID, string> values)
+        {
+            StandardValuesCache standardValuesCache = item.Database.Caches.StandardValuesCache;
+            standardValuesCache.AddStandardValues(item, values);
+        }
+
+        private SafeDictionary<ID, string> ReadStandardValues(ID templateId, Database database, Language language)
+        {
+            SafeDictionary<ID, string> result = new SafeDictionary<ID, string>();
+            Template template = TemplateManager.GetTemplate(templateId, database);
+            if (template == null)
+            {
+                return result;
+            }
+            this.AddStandardValues(template, database, language, result);
+            TemplateList baseTemplates = template.GetBaseTemplates();
+            foreach (Template template2 in baseTemplates)
+            {
+                this.AddStandardValues(template2, database, language, result);
+            }
+            return result;
+        }
+
+        private void AddStandardValues(Template template, Database database, Language language, SafeDictionary<ID, string> result)
+        {
+            ID standardValueHolderId = template.StandardValueHolderId;
+            if (ID.IsNullOrEmpty(standardValueHolderId))
+            {
+                return;
+            }
+            bool? currentValue = Switcher<bool?, LanguageFallbackItemSwitcher>.CurrentValue;
+            Item item;
+            if (currentValue == false)
+            {
+                try
+                {
+                    Switcher<bool?, LanguageFallbackItemSwitcher>.Exit();
+                    item = ItemManager.GetItem(standardValueHolderId, language, Sitecore.Data.Version.Latest, database, SecurityCheck.Disable);
+                    goto IL_5A;
+                }
+                finally
+                {
+                    Switcher<bool?, LanguageFallbackItemSwitcher>.Enter(currentValue);
+                }
+            }
+            item = ItemManager.GetItem(standardValueHolderId, language, Sitecore.Data.Version.Latest, database, SecurityCheck.Disable);
+        IL_5A:
+            if (item == null)
+            {
+                return;
+            }
+            foreach (Field field in item.Fields)
+            {
+                if (!result.ContainsKey(field.ID))
+                {
+                    string value = field.GetValue(false, true);
+                    if (value != null)
+                    {
+                        result[field.ID] = value;
+                    }
+                }
+            }
+            if (!result.ContainsKey(FieldIDs.StandardValueHolderId))
+            {
+                result[FieldIDs.StandardValueHolderId] = standardValueHolderId.ToString();
+            }
+        }
+    }
+}
